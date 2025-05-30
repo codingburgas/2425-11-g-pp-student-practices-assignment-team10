f9634c60f65f77ae974f492af00252548bfb7a8b
GAAtanasov21 <GAAtanasov21@codingburgas.bg>
2025-05-30 10:04:08 +0300

    Fix thank you page

--- flaskProject/form/models.py
+++ flaskProject/form/models.py
@@ -1,30 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-
-from flask_login import UserMixin
-
-from ..auth.models import User
-
-from flaskProject import db
-
-
-class StudentSurveyResponse(db.Model):
-    __tablename__ = 'student_survey_responses'
-    id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False, unique=True)
-    favorite_subject = db.Column(db.String(50), nullable=False)
-    learning_style = db.Column(db.String(50), nullable=False)
-    strength = db.Column(db.String(50), nullable=False)
-    future_study = db.Column(db.String(50), nullable=False)
-    class_behavior = db.Column(db.String(50), nullable=False)
-    activities = db.Column(db.String(50), nullable=False)
-    goal = db.Column(db.String(50), nullable=False)
-    student = db.relationship('User', backref=db.backref('survey_response', uselist=False))
-
-    def __repr__(self):
-        return f'<SurveyResponse student_id={self.student_id}>'
-
-
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from ..auth.models import User
 from flaskProject import db
