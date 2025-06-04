3d7338ce04cc2a8508bafe46be9d3a4a868bdd10
mtnenkov21 <mtnenkov21@codingburgas.bg>
2025-06-04 09:22:49 +0300

    Fix fill a survey button for existing users

--- flaskProject/main/routes.py
+++ flaskProject/main/routes.py
@@ -1,11 +1,13 @@
 from flask import render_template
 from flask_login import login_required, current_user
 from . import main_bp
-from flaskProject.form.models import StudentSurveyResponse
+from flaskProject.form.models import StudentSurveyResponse, TeacherSurveyResponse
+
 
 @main_bp.route('/')
 def index():
     return render_template('index.html')
+
 
 @main_bp.route('/profile')
 @login_required
@@ -13,5 +15,7 @@ def profile():
     has_survey = False
     if current_user.role == 'student':
         has_survey = StudentSurveyResponse.query.filter_by(student_id=current_user.id).first() is not None
+    elif current_user.role == 'teacher':
+        has_survey = TeacherSurveyResponse.query.filter_by(teacher_id=current_user.id).first() is not None
 
     return render_template('profile.html', user=current_user, has_survey=has_survey)
