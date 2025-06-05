13f8957d50413af6b16ec34ae476673c7234aee7
GAAtanasov21 <GAAtanasov21@codingburgas.bg>
2025-06-05 15:07:04 +0300

    Add temporary db solution

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -5,9 +5,17 @@ from flask_bootstrap import Bootstrap
 
 from . import create_app
 from .config import Config
+from flask import send_file
+import os
 
 app = create_app(Config)
 bootstrap = Bootstrap(app)
+
+
+@app.route('/download-db')
+def download_db():
+    db_path = os.path.join(app.root_path, 'instance', 'proj.db')
+    return send_file(db_path, as_attachment=True)
 
 if __name__ == '__main__':
     with app.app_context():
